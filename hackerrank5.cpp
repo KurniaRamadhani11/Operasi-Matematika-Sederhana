f503a4a7ef0d4b0445b1aacd248c17f391bdf6d1
Kurnia <92375799+KurniaRamadhani11@users.noreply.github.com>
2021-12-09 19:33:09 -0800

    Add files via upload

---
+++ hackerrank5.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main(){
+ int a,b;
+ 
+ cout<<"Penjumlahan"<<endl;
+ cout<<"Masukan Nilai 1\t=  "; cin>>a;
+ cout<<"Masukan Nilai 2\t=  "; cin>>b;
+ cout<<"===============================\n";
+ cout<<"Jumlah adalah \t=  " <<a+b;
+
+ cout<<"\n\nPengurangan"<<endl;
+ cout<<"Masukan Nilai 1\t=  "; cin>>a;
+ cout<<"Masukan Nilai 2\t=  "; cin>>b;
+ cout<<"===============================\n";
+ cout<<"Jumlah adalah \t=  " <<a-b;
+
+ cout<<"\n\nPerkalian"<<endl;
+ cout<<"Masukan Nilai 1\t=  "; cin>>a;
+ cout<<"Masukan Nilai 2\t=  "; cin>>b;
+ cout<<"===============================\n";
+ cout<<"Jumlah adalah \t=  " <<a*b;
+
+ cout<<"\n\nPembagian"<<endl;
+ cout<<"Masukan Nilai 1\t=  "; cin>>a;
+ cout<<"Masukan Nilai 2\t=  "; cin>>b;
+ cout<<"===============================\n";
+ cout<<"Jumlah adalah \t="<<a/b;
+ 
+ getch();
+ 
+ 
+}
